89da7ce53f6f40c12a9ef340fbfab6e1a018c7f2
flow119 <simin_shen@163.com>
2018-03-18 21:48:45 +0800

    MonoAdapter的gameObject,transform等成员不再需要写在西瓜里，自动注册。（补充的话在CQ_Behaviour里：RegisterMember注册，DefaultValue赋值）

--- Assets/CQuark/_Unity/CQuarkBehaviour.cs
+++ Assets/CQuark/_Unity/CQuarkBehaviour.cs
@@ -50,25 +50,46 @@ public class CQuarkBehaviour : MonoBehaviourAdapter {
 			return;
 		}
 		content = new CQ_Content(true);
+
+		//TODO 最好在编译的时候就做，不要在实例化的时候做
+		RegisterMember("gameObject", typeof(GameObject));
+		RegisterMember("transform", typeof(Transform));
+
 		inst = type._class.New(content, null).value as CQuark.CQClassInstance;
-		content.CallType = inst.type;
-		content.CallThis = inst;
-//		if (!inst.member.ContainsKey ("gameObject")){
-//			CQ_Content.Value val = new CQ_Content.Value ();
-//			val.type = typeof(GameObject);
-//			val.value = this.gameObject;
-//			inst.member.Add ("gameObject", val);
-//		}
-//		if (!inst.member.ContainsKey ("transform")) {
-//			CQ_Content.Value val = new CQ_Content.Value ();
-//			val.type = typeof(Transform);
-//			val.value = this.transform;
-//			inst.member.Add ("transform", val);
-//		}
-		inst.member["gameObject"].value = this.gameObject;
-//		inst.member ["transform"].value = this.transform;
+		DefaultValue("gameObject", typeof(GameObject), this.gameObject);
+		DefaultValue("transform", typeof(Transform), this.transform);
 	}
 
+	CQ_Value DefaultValue(string name, CQ_Type type, Object obj){
+		CQ_Value val = new CQ_Value ();
+		val.type = type;
+		val.value = obj;
+		inst.member[name] = val;
+		return val;
+	}
+
+	void RegisterMember(string name, CQ_Type cqtype){
+		Class_CQuark cclass = (Class_CQuark)type._class;
+		if(!cclass.members.ContainsKey(name)){
+			Class_CQuark.Member m = new Class_CQuark.Member();
+			m.bPublic = true;
+			m.bReadOnly = true;
+			m.type = AppDomain.GetType(cqtype);
+			cclass.members.Add(name, m);
+		}
+	}
+//	void AddMember(string name, CQ_Type type, Object obj){
+//		if(!inst.type.members.ContainsKey(name)){
+//			Class_CQuark.Member m = new Class_CQuark.Member();
+//			m.bPublic = true;
+//			m.bReadOnly = true;
+//			inst.type.members.Add(name, m);
+//		}
+//		CQ_Value val = new CQ_Value ();
+//		val.type = type;
+//		val.value = obj;
+//		inst.member.Add (name, val);
+//	}
 
 	protected override void CallScript(string methodName, bool useCoroutine){
 		Class_CQuark cclass = type._class as Class_CQuark;
