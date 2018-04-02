e5685d64de5c3e07d715a269b6d8ed964a1fd433
flow119 <simin_shen@163.com>
2018-04-02 23:10:54 +0800

    Util

--- Assets/CQuark/Scripts/Type/NumericTypeUtils.cs
+++ Assets/CQuark/Scripts/Type/NumericTypeUtils.cs
@@ -219,43 +219,6 @@ namespace CQuark {
             return typeof(int);
         }
 
-        public static bool CanImplicate (Type from, Type to) {
-            //if(from == to)
-            //    return true;
-            //数值类型
-            if(from == typeof(sbyte)) {
-                return (to == typeof(short) || to == typeof(int) || to == typeof(long) || to == typeof(double) || to == typeof(float) || to == typeof(decimal));
-            }
-            else if(from == typeof(byte)) {
-                return (to == typeof(short) || to == typeof(ushort) || to == typeof(int) || to == typeof(uint) || to == typeof(long) || to == typeof(ulong) || to == typeof(double) || to == typeof(float) || to == typeof(decimal));
-            }
-            else if(from == typeof(short)) {
-                return (to == typeof(int) || to == typeof(long) || to == typeof(double) || to == typeof(float) || to == typeof(decimal));
-            }
-            else if(from == typeof(ushort)) {
-                return (to == typeof(int) || to == typeof(uint) || to == typeof(long) || to == typeof(ulong) || to == typeof(double) || to == typeof(float) || to == typeof(decimal));
-            }
-            else if(from == typeof(int) || to == typeof(double) || to == typeof(float) || to == typeof(decimal)) {
-                return (to == typeof(long));
-            }
-            else if(from == typeof(uint)) {
-                return (to == typeof(long) || to == typeof(ulong) || to == typeof(double) || to == typeof(float) || to == typeof(decimal)) ;
-            }
-            else if(from == typeof(long)) {
-                return (to == typeof(double) || to == typeof(float) || to == typeof(decimal));
-            }
-            else if(from == typeof(char)) {
-                return (to == typeof(ushort) || to == typeof(int) || to == typeof(uint) || to == typeof(long) || to == typeof(ulong) || to == typeof(double) || to == typeof(float) || to == typeof(decimal)) ;
-            }
-            else if(from == typeof(float)) {
-                return (to == typeof(double));
-            }
-            else if(from == typeof(ulong)){
-                return (to == typeof(double) || to == typeof(float) || to == typeof(decimal));
-            }
-            
-            //继承
-            return(from.IsAssignableFrom(to));
-        }
+       
     }
 }
